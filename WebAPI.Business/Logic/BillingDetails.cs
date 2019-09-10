81461cf523a7c1820f2aef78a597dbc353ad8dac
Rahul Rajput <rahul>
2019-09-11 05:04:37 +0530

    Added some business layer comments/logic

--- WebAPI.Business/Logic/BillingDetails.cs
+++ WebAPI.Business/Logic/BillingDetails.cs
@@ -1,21 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using WebAPI.Business.Interface;
 using WebAPI.Business.Models;
 using WebAPI.DataServices.Contracts;
 
 namespace WebAPI.Business.Logic
 {
+    /// <summary>
+    /// Billing Details Business layer implementation.
+    /// </summary>
     public class BillingDetails : IBillingDetails
     {
+        /// <summary>
+        /// interface for the Billing repo
+        /// </summary>
         private readonly IBillingRepository iBillingRepository;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="billingRepository"></param>
         public BillingDetails(IBillingRepository billingRepository)
         {
             iBillingRepository = billingRepository;
         }
 
+        /// <summary>
+        /// Get the billing details
+        /// </summary>
+        /// <returns>IEnumerable list of billing table objects </returns>
         public IEnumerable<BillingDetailModel> Get()
         {
             return iBillingRepository.Get().Select(x => new BillingDetailModel {
@@ -27,6 +40,11 @@ namespace WebAPI.Business.Logic
             }).ToList();
         }
 
+        /// <summary>
+        /// Get the billing details for the given billing id
+        /// </summary>
+        /// <param name="id">billing id</param>
+        /// <returns>returns billing class object</returns>
         public BillingDetailModel Get(long id)
         {
             var DbBillingDetails = iBillingRepository.Get(id);
@@ -40,6 +58,11 @@ namespace WebAPI.Business.Logic
             };
         }
 
+
+        /// <summary>
+        /// Saves the billing details 
+        /// </summary>
+        /// <param name="billingDetails">billing class object model</param>
         public void Post(BillingDetailModel billingDetails)
         {
             iBillingRepository.Save(new DataServices.Table.BillingDetails {
@@ -50,6 +73,11 @@ namespace WebAPI.Business.Logic
             });
         }
 
+        /// <summary>
+        /// Update the billing details
+        /// </summary>
+        /// <param name="Id">Billing Id</param>
+        /// <param name="billingDetails">billing class object model</param>
         public void Put(BillingDetailModel billingDetails)
         {
             iBillingRepository.Put(new DataServices.Table.BillingDetails
@@ -62,6 +90,10 @@ namespace WebAPI.Business.Logic
             });
         }
 
+        /// <summary>
+        /// Remove the billing details from the system
+        /// </summary>
+        /// <param name="id">billing details id</param>
         public void Delete(long id)
         {
             iBillingRepository.Delete(id);
